cf6fbe6353d358ffb3c30ad6511c5c0131494fd5
moon9ibulbul <62831103+moon9ibulbul@users.noreply.github.com>
2025-11-19 19:21:22 +0700

    Add missing Compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,9 +62,11 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.foundation:foundation")
     implementation("androidx.compose.material3:material3:1.2.1")
     implementation("com.google.android.material:material:1.12.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
